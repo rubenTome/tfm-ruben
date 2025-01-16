9433fcca24c67b1d785e7e6a9e14ed59bafe64c4
rubenTome <rubentome27@gmail.com>
2025-01-16 16:31:28 +0100

    añadidos campos de formulario

--- tfm-frontend/src/app/pages/home/home.component.ts
+++ tfm-frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,46 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  paramsForm = new FormGroup({
+    dataset: new FormControl(''),
+    nFeatures: new FormControl(''),
+    precision: new FormControl(''),
+    codecarbon: new FormControl(''),
+    kFolds: new FormControl(''),
+    reps: new FormControl(''),
+    alpha: new FormControl(''),
+    implementation: new FormControl(''),
+
+  });
+
+  datasetsOptions = [
+    "Colon", 
+    "Leukemia", 
+    "Lung",
+    "Lymphoma",
+    "Gina",
+    "Gisette",
+    "Dexter",
+    "Madelon",
+  ]
+  nFeaturesOptions = [
+    "10",
+    "20",
+    "30",
+    "40",
+  ]
+  precisionOptions = [
+    "16",
+    "32",
+    "64",
+  ]
+}
