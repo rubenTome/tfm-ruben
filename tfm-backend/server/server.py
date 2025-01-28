25b88609c5a6049da52b92ca70ad9f3afbd23038
rubenTome <rubentome27@gmail.com>
2025-01-28 12:30:09 +0100

    server mod

--- tfm-backend/server/server.py
+++ tfm-backend/server/server.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 from script_alpha import run_experiment
 app = FastAPI()
 
-# http://127.0.0.1:8000/experiment/colon/10/32/3/5/0.25/25/linear/codecarbon_tracking
-@app.get("/experiment/{dataset}/{nFeat}/{prec}/{kFolds}/{reps}/{alpha}/{wait}/{implementation}/{codecarbon}")
+@app.get("/experiment/")
 async def expermient(dataset: str, nFeat: str, prec: str, kFolds: str, reps: str, alpha: str, wait: str, implementation: str, codecarbon: str):
-    run_experiment(dataset, nFeat, prec, kFolds, reps, alpha, wait, implementation, codecarbon)
+    #run_experiment(dataset, nFeat, prec, kFolds, reps, alpha, wait, implementation, codecarbon)
+    print([dataset, nFeat, prec, kFolds, reps, alpha, wait, implementation, codecarbon])
     return {"success": [dataset, nFeat, prec, kFolds, reps, alpha, wait, implementation, codecarbon]}
