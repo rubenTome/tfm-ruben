9dfdae4529aacd9b6370c59b10aeee4969b6065a
AbelJuncal <abel.juncal@udc.es>
2025-03-20 13:03:00 +0100

    configurado docker

--- tfm-backend/server/server.py
+++ tfm-backend/server/server.py
@@ -11,7 +11,7 @@ import firebase_admin
 from firebase_admin import db
 
 #DEBE UBICARSE LA CLAVE PRIADA DE FIREBASE EN LA CARPETA TFM-DB
-cred = firebase_admin.credentials.Certificate(Path("../../tfm-db/private_key.json"))
+cred = firebase_admin.credentials.Certificate(Path("private_key.json"))
 firebase_admin.initialize_app(cred, {'databaseURL': "https://tfm-bd-3e179-default-rtdb.europe-west1.firebasedatabase.app/"})
 ref = db.reference("/")
 
